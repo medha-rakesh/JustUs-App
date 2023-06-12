c672336954bb5cad8c5edb2b75322328a944424b
medha rakesh <medha.rakesh08@gmail.con>
2023-06-12 11:13:38 -0700

    Initial Commit

---
+++ JustUs App/JustUs_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustUs_AppApp.swift
+//  JustUs App
+//
+//  Created by MahaPeriva on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct JustUs_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
